eaabe7141ede40bdeba82b38e85a4db4d6118795
john <johnmaluki58@gmail.com>
2023-06-21 15:47:25 +0300

    Complete assignment on The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  init();
+});
+
+const init = () => {
+  const p = document.getElementById("text");
+  p.textContent = "This is really cool!";
+};
